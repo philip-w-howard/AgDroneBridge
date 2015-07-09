8fa7dced2e9890af2a57335f96fd494084d736df
Philip W. Howard <phil.w.howard@gmail.com>
2015-07-09 09:16:05 -0700

    Initial Release

---
+++ AgDroneBridge/IPServer.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading;
+
+public class IPServer
+{
+    public IPServer(AgDroneBridge.MainWindow app)
+	{
+        mApp = app;
+	}
+
+    private int mLocalPort;
+    private int mRemotePort;
+    private string mRemoteAddress;
+    private Thread mServerThread;
+    private Thread mClientThread;
+    private AgDroneBridge.MainWindow mApp;
+    private Stream mRemoteStream;
+    private Socket mLocalSocket;
+    private Int64 mADSent;
+    private Int64 mADReceived;
+    private Int64 mMPSent;
+    private Int64 mMPReceived;
+
+    public void Start(int localPort, string remoteAddress, int remotePort)
+    {
+        mLocalPort = localPort;
+        mRemotePort = remotePort;
+        mRemoteAddress = remoteAddress;
+
+        mServerThread = new Thread(new ThreadStart(Server));
+        mServerThread.Start();
+
+        mClientThread = new Thread(new ThreadStart(Client));
+        mClientThread.Start();
+    }
+
+    public void Stop()
+    {
+        mServerThread.Abort();
+       // mServerThread.Join();
+
+        mClientThread.Abort();
+       // mServerThread.Join();
+    }
+    public void Server()
+    {
+        try {
+            IPAddress ipAd = IPAddress.Parse("127.0.0.1");
+             // use local m/c IP address, and 
+             // use the same in the client
+
+    /* Initializes the Listener */
+            TcpListener myList=new TcpListener(ipAd, mLocalPort);
+
+    /* Start Listeneting at the specified port */        
+            myList.Start();
+
+            Console.WriteLine("The server is running at port " + mLocalPort);    
+            Console.WriteLine("The local End point is  :" + 
+                              myList.LocalEndpoint );
+            Console.WriteLine("Waiting for a connection.....");
+
+            mLocalSocket = myList.AcceptSocket();
+            Console.WriteLine("Connection accepted from " + mLocalSocket.RemoteEndPoint);
+        
+            byte[] buffer=new byte[200];
+            int len;
+            while (true)
+            {
+                //lock (mLocalSocket)
+                {
+                    len = mLocalSocket.Receive(buffer);
+                }
+
+                mMPReceived += len;
+                mApp.SetMPReceived(mMPReceived);
+
+                try
+                {
+                    if (mRemoteStream != null)
+                    {
+                        //lock (mRemoteStream)
+                        {
+                            mRemoteStream.Write(buffer, 0, len);
+                        }
+                        mADSent += len;
+                        mApp.SetADSent(mADSent);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error sending to AD " + e.ToString());
+                }
+
+            }
+            //ASCIIEncoding asen=new ASCIIEncoding();
+            //s.Send(asen.GetBytes("The string was recieved by the server."));
+            //Console.WriteLine("\nSent Acknowledgement");
+    /* clean up */            
+            //s.Close();
+            //myList.Stop();
+            
+        }
+        catch (Exception e) {
+            Console.WriteLine("Error..... " + e.StackTrace);
+        }    
+    }
+
+    public void Client() {
+        while (true)
+        {
+
+            try
+            {
+                TcpClient tcpclnt = new TcpClient();
+                Console.WriteLine("Connecting to AgDrone at " + mRemoteAddress + " " + mRemotePort);
+
+                tcpclnt.Connect(mRemoteAddress, mRemotePort);
+                // use the ipaddress as in the server program
+
+                Console.WriteLine("AgDrone Connected");
+                Console.Write("Enter the string to be transmitted : ");
+
+                mRemoteStream = tcpclnt.GetStream();
+
+                byte[] buffer = new byte[200];
+                int len;
+                while (true) // (tcpclnt.Available != 0)
+                {
+                    //lock (mRemoteStream)
+                    {
+                        len = mRemoteStream.Read(buffer, 0, 200);
+                    }
+                    mADReceived += len;
+                    mApp.SetADReceived(mADReceived);
+                    try
+                    {
+                        //lock (mLocalSocket)
+                        {
+                            mLocalSocket.Send(buffer, len, SocketFlags.None);
+                        }
+                        mMPSent += len;
+                        mApp.SetMPSent(mMPSent);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Error sending to MP " + e.ToString());
+                    }
+
+                }
+
+                //tcpclnt.Close();
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Error..... " + e.StackTrace);
+            }
+        }
+    }
+
+
+}
---
+++ AgDroneBridge/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AgDroneBridge
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            mIPServer = new IPServer(this);
+        }
+
+        private IPServer mIPServer;
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (StartButton.Content.Equals("Stop"))
+            {
+                StartButton.Content = "Start";
+                mIPServer.Stop();
+            }
+            else 
+            {
+                StartButton.Content = "Stop";
+                mIPServer.Start(Convert.ToInt32(LocalPort.Text), AgDroneAddress.Text, Convert.ToInt32(AgDronePort.Text));
+            }
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        protected delegate void UpdateTextCallback(string message);
+
+        public void SetMPReceived(Int64 count)
+        {
+            MPReceived.Dispatcher.Invoke(
+                new UpdateTextCallback(UpdateMPReceived),
+                new object[] { count.ToString() }
+            );
+        }
+
+        public void SetADReceived(Int64 count)
+        {
+            ADReceived.Dispatcher.Invoke(
+                new UpdateTextCallback(UpdateADReceived),
+                new object[] { count.ToString() }
+            );
+        }
+        public void SetADSent(Int64 count)
+        {
+            ADSent.Dispatcher.Invoke(
+                new UpdateTextCallback(UpdateADSent),
+                new object[] { count.ToString() }
+            );
+        }
+        public void SetMPSent(Int64 count)
+        {
+            MPSent.Dispatcher.Invoke(
+                new UpdateTextCallback(UpdateMPSent),
+                new object[] { count.ToString() }
+            );
+        }
+
+        protected void UpdateMPReceived(string text)
+        {
+            MPReceived.Text = text;
+        }
+        protected void UpdateADReceived(string text)
+        {
+            ADReceived.Text = text;
+        }
+        protected void UpdateADSent(string text)
+        {
+            ADSent.Text = text;
+        }
+        protected void UpdateMPSent(string text)
+        {
+            MPSent.Text = text;
+        }
+    }
+}
